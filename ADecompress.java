df2402f99418e379b053bdcb2eed6c00ec5e9353
SeeNot <142214624+SeeNot@users.noreply.github.com>
2024-04-18 12:31:49 +0300

    Pievienots makets

--- ADecompress.java
+++ ADecompress.java
@@ -1,17 +1,14 @@
 import java.util.Scanner;
 
-public class ACompress {
+public class ADecompress {
 
-    public static void compress(){
+    public static void deCompress(){
         String sourceFile, resultFile;
         Scanner sc = new Scanner(System.in);
         System.out.print("source file name: ");
         sourceFile = sc.next();
         System.out.print("archive name: ");
         resultFile = sc.next();
-
-
-
 
 
         sc.close();
--- Main.java
+++ Main.java
@@ -1,18 +1,106 @@
+// 000RDB000 Jānis Programmētājs
+// 111RDB111 Ilze Programmētāja
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
 
 
-class Main {
-
-    public static void main(String[] args)
-    {
-
-
-
-
-
+public class Main {
 
 
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String choiseStr;
+        String sourceFile, firstFile, secondFile;
+
+        loop: while (true) {
+
+            choiseStr = sc.next();
+
+            switch (choiseStr) {
+                case "comp":
+                    ACompress.compress();
+                    break;
+                case "decomp":
+
+                    ADecompress.deCompress();
+                    break;
+                case "size":
+                    System.out.print("file name: ");
+                    sourceFile = sc.next();
+                    size(sourceFile);
+                    break;
+                case "equal":
+                    System.out.print("first file name: ");
+                    firstFile = sc.next();
+                    System.out.print("second file name: ");
+                    secondFile = sc.next();
+                    System.out.println(equal(firstFile, secondFile));
+                    break;
+                case "about":
+                    about();
+                    break;
+                case "exit":
+                    break loop;
+            }
+        }
+
+        sc.close();
+    }
+
+
+    public static void size(String sourceFile) {
+        try {
+            FileInputStream f = new FileInputStream(sourceFile);
+            System.out.println("size: " + f.available());
+            f.close();
+        }
+        catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
 
     }
 
+    public static boolean equal(String firstFile, String secondFile) {
+        try {
+            FileInputStream f1 = new FileInputStream(firstFile);
+            FileInputStream f2 = new FileInputStream(secondFile);
+            int k1, k2;
+            byte[] buf1 = new byte[1000];
+            byte[] buf2 = new byte[1000];
+            do {
+                k1 = f1.read(buf1);
+                k2 = f2.read(buf2);
+                if (k1 != k2) {
+                    f1.close();
+                    f2.close();
+                    return false;
+                }
+                for (int i=0; i<k1; i++) {
+                    if (buf1[i] != buf2[i]) {
+                        f1.close();
+                        f2.close();
+                        return false;
+                    }
+
+                }
+            } while (!(k1 == -1 && k2 == -1));
+            f1.close();
+            f2.close();
+            return true;
+        }
+        catch (IOException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+
+    public static void about() {
+        // TODO insert information about authors
+        System.out.println("000RDB000 Jānis Programmētājs");
+        System.out.println("111RDB111 Ilze Programmētāja");
+    }
 }
+
