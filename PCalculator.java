d57dd27a4b89e25f62f122627c1c3b8bcfbe6d00
SeeNot <142214624+SeeNot@users.noreply.github.com>
2024-04-25 11:38:10 +0300

    Added the basis of the file reader

--- PCalculator.java
+++ PCalculator.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.LinkedList;
 
@@ -5,15 +9,25 @@ public class PCalculator {
 
     public static void Calculate(String FileName){
 
-        HashMap<Character,int[]> finalTabel = new HashMap<>();
-        HashMap<Character,Integer> tabel = new HashMap<>();
-
-
-
-
+        HashMap<Character,int[]> finalOddsTabel = new HashMap<>();
+        HashMap<Character,Integer> charCounter = new HashMap<>();
 
         long charAmount = 0;
 
+
+        try {
+            BufferedReader br = new BufferedReader
+                    (new InputStreamReader
+                            (new FileInputStream(FileName), "UTF-8"));
+            String line = br.readLine();
+
+
+
+
+            br.close();
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
 
 
 
