cf3e2f053b8ef099702963d2c030734e9c84ad1b
Em1lss <ozolins35@gmail.com>
2024-04-25 11:39:55 +0300

    added method probabilityCalculator

--- PCalculator.java
+++ PCalculator.java
@@ -31,13 +31,24 @@ public class PCalculator {
 
 
 
-        
-
 
 
 
 
 
     }
+    public static void probabilityCalculator(HashMap<Character,Integer> ptabel) {
+        String sc = "asdasd";
+        ptabel.put('a', 2);
+
+        for (int i = 0; i <= sc.length(); i++) {
+            if (ptabel.containsKey(sc.charAt(i))) {
+                ptabel.replace(sc.charAt(i), ptabel.get(sc.charAt(i)), ptabel.get(sc.charAt(i))+1);
+            }
+            else ptabel.put(sc.charAt(i), 1);
+        }
+    }
+
+
 
 }
