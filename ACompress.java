e431e117ac0a664b7be53cf60c0aed47ac93bca2
SeeNot <142214624+SeeNot@users.noreply.github.com>
2024-04-25 13:34:21 +0300

    Refactor to make it easier to use

--- ACompress.java
+++ ACompress.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class ACompress {
@@ -9,10 +14,40 @@ public class ACompress {
         sourceFile = sc.next();
         System.out.print("archive name: ");
         resultFile = sc.next();
+        long maxValue = Long.MAX_VALUE;
+        long minValue = 0L;
+        long halfValue = maxValue/2;
+        long quarterValue = maxValue/4;
 
 
-        PCalculator.Calculate(sourceFile);
+        HashMap<Character,Integer> charCounter = new HashMap<>();
 
+        long charAmount = 0;
+
+        try {
+            BufferedReader br = new BufferedReader
+                    (new InputStreamReader
+                            (new FileInputStream(sourceFile), StandardCharsets.UTF_8));
+            String line;
+            while ((line = br.readLine()) != null) {
+                PCalculator.probabilityCalculator(charCounter, line);
+                charAmount += line.length();
+            }
+
+            br.close();
+
+
+            HashMap<Character,long[]> oddsTable = PCalculator.recalculateOddsTabel(charCounter, charAmount, Long.MAX_VALUE);
+
+            for (HashMap.Entry<Character,long[]> entry : oddsTable.entrySet()) {
+                System.out.println(entry.getKey() + " " + entry.getValue()[0] + " " + entry.getValue()[1]);
+
+            }
+
+
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
 
 
 
--- PCalculator.java
+++ PCalculator.java
@@ -1,55 +1,7 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 
 public class PCalculator {
 
-    public static HashMap<Character,long[]> Calculate(String FileName){
-
-        HashMap<Character,long[]> finalOddsTabel = new HashMap<>();
-        HashMap<Character,Integer> charCounter = new HashMap<>();
-
-        long charAmount = 0;
-
-        try {
-            BufferedReader br = new BufferedReader
-                    (new InputStreamReader
-                            (new FileInputStream(FileName), StandardCharsets.UTF_8));
-            String line;
-            while ((line = br.readLine()) != null) {
-                probabilityCalculator(charCounter, line);
-                charAmount += line.length();
-            }
-
-            long iedalasvertiba = Long.MAX_VALUE/charAmount;
-            long apaksejaVertiba = 0;
-            for (HashMap.Entry<Character, Integer> entry : charCounter.entrySet()) {
-
-                long[] limiti = {apaksejaVertiba, apaksejaVertiba + iedalasvertiba*entry.getValue()};
-                finalOddsTabel.put(entry.getKey(), limiti);
-                apaksejaVertiba += iedalasvertiba*entry.getValue();
-
-            }
-
-
-
-
-
-
-            br.close();
-
-
-
-        } catch (Exception e){
-            System.out.println(e.getMessage());
-        }
-
-
-        return finalOddsTabel;
-
-    }
     public static void probabilityCalculator(HashMap<Character,Integer> ptabel, String line) {
 
         for (int i = 0; i < line.length(); i++) {
@@ -59,6 +11,27 @@ public class PCalculator {
             else ptabel.put(line.charAt(i), 1);
         }
     }
+
+    public static HashMap<Character,long[]> recalculateOddsTabel(HashMap<Character,Integer> ptabel,long charAmount , long maxvalue ) {
+        HashMap<Character,long[]> finalOddsTabel = new HashMap<>();
+
+        long iedalasvertiba = maxvalue/charAmount;
+        long apaksejaVertiba = 0;
+        for (HashMap.Entry<Character, Integer> entry : ptabel.entrySet()) {
+
+            long[] limiti = {apaksejaVertiba, apaksejaVertiba + iedalasvertiba*entry.getValue()};
+            finalOddsTabel.put(entry.getKey(), limiti);
+            apaksejaVertiba += iedalasvertiba*entry.getValue();
+
+        }
+
+
+        return finalOddsTabel;
+
+
+
+    }
+
 
 
 
